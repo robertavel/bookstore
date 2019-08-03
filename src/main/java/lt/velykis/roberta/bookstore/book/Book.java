2139db538d57b8311459b14ccaf543de7d3c03d8
Roberta Velykiene <roberta.velykiene@gmail.com>
2019-08-03 22:37:22 +0300

    include object type in json

--- src/main/java/lt/velykis/roberta/bookstore/book/Book.java
+++ src/main/java/lt/velykis/roberta/bookstore/book/Book.java
@@ -1,11 +1,24 @@
 package lt.velykis.roberta.bookstore.book;
 
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonSubTypes.Type;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 
+
+@JsonTypeInfo(
+        use = JsonTypeInfo.Id.NAME,
+        include = JsonTypeInfo.As.PROPERTY,
+        property = "type")
+@JsonSubTypes({
+        @Type(value = Book.class, name = "book"),
+        @Type(value = AntiqueBook.class, name = "antiqueBook"),
+        @Type(value = Journal.class, name = "journal")
+})
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
